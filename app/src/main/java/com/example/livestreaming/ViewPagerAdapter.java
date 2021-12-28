c5584283f80b7d27e95343f4610822daa219efa8
AnXiao <anxiao@whu.edu.cn>
2021-12-28 17:53:35 +0800

    files changed

---
+++ app/src/main/java/com/example/livestreaming/ViewPagerAdapter.java
@@ -0,0 +1,54 @@
+package com.example.livestreaming;
+
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>{
+    private final List<String> uri = new ArrayList<>();
+    public ViewPagerAdapter(){
+        uri.add("android.resource://" + MainActivity.PACKAGE_NAME + "/" + R.raw.vertical);
+        uri.add("android.resource://" + MainActivity.PACKAGE_NAME + "/" + R.raw.horizontal);
+        uri.add("android.resource://" + MainActivity.PACKAGE_NAME + "/" + R.raw.vertical);
+        uri.add("android.resource://" + MainActivity.PACKAGE_NAME + "/" + R.raw.horizontal);
+        uri.add("android.resource://" + MainActivity.PACKAGE_NAME + "/" + R.raw.vertical);
+        uri.add("android.resource://" + MainActivity.PACKAGE_NAME + "/" + R.raw.horizontal);
+        uri.add("android.resource://" + MainActivity.PACKAGE_NAME + "/" + R.raw.vertical);
+        uri.add("android.resource://" + MainActivity.PACKAGE_NAME + "/" + R.raw.horizontal);
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.video_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewPagerAdapter.ViewHolder holder, int position) {
+        holder.videoView.setVideoURI(Uri.parse(uri.get(position)));
+        holder.videoView.requestFocus();
+        holder.videoView.start();
+    }
+
+    @Override
+    public int getItemCount() {
+        return uri.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        VideoView videoView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            videoView = itemView.findViewById(R.id.video_view);
+        }
+    }
+}
+
